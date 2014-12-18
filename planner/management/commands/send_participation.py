85368f53cb40f972547195d4d7db3eef11afb22d
danieka <danieka@kth.se>
2014-12-18 11:10:06 +0100

    Added command for sending updated participations.

--- planner/management/commands/send_participation.py
+++ planner/management/commands/send_participation.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand, CommandError
-from planner.tasks import *
+from planner.tasks import send_email_participation
 
 class Command(BaseCommand):
     help = 'Closes the specified poll for voting'
