bfae494e758587f5d55d081a6e8ea973ab6424b6
danieka <danieka@kth.se>
2014-12-18 11:15:28 +0100

    Fail bugfix.

--- planner/management/commands/send_updated_participations.py
+++ planner/management/commands/send_updated_participations.py
@@ -1,8 +1,8 @@
 from django.core.management.base import BaseCommand, CommandError
-from planner.tasks import send_updated_participation
+from planner.tasks import send_updated_participations
 
 class Command(BaseCommand):
     help = 'Closes the specified poll for voting'
 
     def handle(self, *args, **options):
-        send_updated_participation()
+        send_updated_participations()
